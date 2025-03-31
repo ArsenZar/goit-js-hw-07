5c008544b1720748cbb98c11144d7be6c6bbc67c
ArsenZar <arsenzar@gmail.com>
2025-04-01 00:44:10 +0300

    fix append elem (becouse thay add inside h1 tag)

--- js/task-1.js
+++ js/task-1.js
@@ -1,13 +1,14 @@
-const mainTitle = document.querySelector(".mainH1");
+const mainTitle = document.querySelector(".firstSection");
 
 const heading = document.createElement("p");
 heading.classList.add("title");
 heading.style.fontSize = "12px";
 heading.textContent = "I add this text from JS";
-mainTitle.append(heading);
 console.log(heading); // <h1 class="title">This is a heading</h1>
 
 const image = document.createElement("img");
 image.src = "https://picsum.photos/id/11/320/240";
 image.alt = "Nature";
 console.log(image); // <img src="https://picsum.photos/id/11/320/240" alt="Nature" />
+
+mainTitle.append(heading, image);
