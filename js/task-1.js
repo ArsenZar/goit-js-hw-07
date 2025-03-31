3c157e373882ff18780edde9ce302aaefd7862ff
ArsenZar <arsenzar@gmail.com>
2025-03-31 20:22:22 +0300

    work with data-attribute

--- js/task-1.js
+++ js/task-1.js
@@ -1,19 +1,6 @@
-/* work with classList */
+const saveBtn = document.querySelector('.editor button[data-action="save"]');
+const closeBtn = document.querySelector('.editor button[data-action="close"]');
 
-const link = document.querySelector(".link");
-console.log(link.classList);
+console.log(saveBtn.dataset.action); //save
+console.log(closeBtn.dataset.action); //close
 
-const hasActiveClass = link.classList.contains("is-active");
-console.log(`hasActiveClass - ${hasActiveClass}`);
-
-link.classList.add("special");
-console.log(link.classList);
-
-link.classList.remove("is-active");
-console.log(link.classList);
-
-link.classList.toggle("is-active");
-console.log(link.classList);
-
-link.classList.replace("special", "regular");
-console.log(link.classList);
