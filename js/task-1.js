5866798a932fafe1f456ca228eb6b3de1da287d9
ArsenZar <arsenzar@gmail.com>
2025-04-01 15:46:56 +0300

    listener input (looks like reactiv)))))

--- js/task-1.js
+++ js/task-1.js
@@ -78,5 +78,11 @@ function setOutput(event) {
   valueOutput.textContent = selectedOptionValue;
 }
 
+// input
 
+const textInput = document.querySelector(".text-input");
+const output = document.querySelector(".output");
 
+textInput.addEventListener("input", (event) => {
+  output.textContent = event.currentTarget.value;
+});
