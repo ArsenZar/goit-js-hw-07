dbede2926340457bc38ec33341d306b6de9e8c40
ArsenZar <arsenzar@gmail.com>
2025-04-01 15:42:48 +0300

    listener change (for example select list)

--- js/task-1.js
+++ js/task-1.js
@@ -54,11 +54,28 @@ function handleSubmit(event) {
   const password = form.elements.password.value;
   
   if (login === "" || password === "") {
-    return console.log("Please fill in all the fields!");
+    return //console.log("Please fill in all the fields!");
   }
 
-  console.log(`Login: ${login}, Password: ${password}`);
+  //console.log(`Login: ${login}, Password: ${password}`);
   form.reset();
+}
+
+// select
+
+const select = document.querySelector(".pizza-select");
+const textOutput = document.querySelector(".text-output");
+const valueOutput = document.querySelector(".value-output");
+
+select.addEventListener("change", setOutput);
+
+function setOutput(event) {
+  const selectedOptionValue = event.currentTarget.value;
+  const selectedOptionIndex = event.currentTarget.selectedIndex;
+  const selectedOptionText = event.currentTarget.options[selectedOptionIndex].text;
+
+  textOutput.textContent = selectedOptionText;
+  valueOutput.textContent = selectedOptionValue;
 }
 
 
