5767b610907695ac5d9edd7e1805556b7ae36aa8
ArsenZar <arsenzar@gmail.com>
2025-04-02 14:12:12 +0300

    task-5 ready

--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,10 @@
+const bg = document.querySelector("body");
+const button = document.querySelector(".change-color");
+
+button.addEventListener("click", () => {
+  bg.style.backgroundColor = getRandomHexColor()
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
