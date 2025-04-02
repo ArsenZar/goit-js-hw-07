1664839f066f630f06d94da549ef0adacecf9893
ArsenZar <arsenzar@gmail.com>
2025-04-02 13:53:23 +0300

    task-4 ready

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,24 @@
+const registerForm = document.querySelector(".login-form");
+ 
+ registerForm.addEventListener("submit", handleSubmit);
+ 
+function handleSubmit(event) {
+    event.preventDefault();
 
+    const form = event.target;
+    const email = form.elements.email.value;
+    const pass = form.elements.password.value;
+    console.log(email);
+
+    const elements = {}
+
+   
+   if (email.trim() === "" || pass.trim() === "") {
+     return alert("Please fill in all the fields!");
+   } else {
+       elements.email = email;
+       elements.pass = pass;
+   }
+    form.reset();
+    console.log(elements);
+ }
